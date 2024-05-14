dcf1f889a469d735118c2713d9a90704f898c73c
rmdluo <rmdluo@gmail.com>
2024-05-13 21:23:24 -0400

    added functionality to turn towards face and walk to it
    has issues with:
     - locating faces
     - adjusting to turn towards face properly
     - walking distance

--- examples/14_follow_voice.py
+++ examples/14_follow_voice.py
@@ -3,9 +3,22 @@ from pidog import Pidog
 from time import sleep
 from vilib import Vilib
 from preset_actions import bark
+import math
 
 my_dog = Pidog()
 sleep(0.1)
+
+def head_nod(step):
+    y = 0
+    r = 0
+    p = 30
+    angs = []
+    for i in range(20):
+        r = round(10*math.sin(i*0.314), 2)
+        p = round(20*math.sin(i*0.314) + 10, 2)
+        angs.append([y, r, p])
+
+    my_dog.head_move(angs*step, pitch_comp=-40, immediately=False, speed=80)
 
 def face_track():
     Vilib.camera_start(vflip=False, hflip=False)
@@ -19,8 +32,9 @@ def face_track():
     flag = False
     direction = 0
 
-    my_dog.do_action('sit', speed=50)
-    my_dog.head_move([[yaw, 0, pitch]], pitch_comp=-40, immediately=True, speed=80)
+    # my_dog.do_action('sit', speed=50)
+    my_dog.do_action('stand', speed=50)
+    my_dog.head_move([[yaw, 0, pitch]], pitch_comp=0, immediately=True, speed=80)
     my_dog.wait_all_done()
     sleep(0.5)
     # Cleanup sound detection by servos moving
@@ -43,7 +57,7 @@ def face_track():
                     yaw = 360 - direction
                     if yaw > 80:
                         yaw = 80
-                my_dog.head_move([[yaw, 0, 0]], pitch_comp=-40, immediately=True, speed=80)
+                my_dog.head_move([[yaw, 0, 0]], pitch_comp=0, immediately=True, speed=80)
                 my_dog.wait_head_done()
                 sleep(0.05)
 
@@ -55,10 +69,8 @@ def face_track():
                     pitch_updates = [i for i in range(80, -40, -10)]
                     # move head from up to down, looking for person
                     for new_pitch in pitch_updates:
-                        my_dog.head_move([[yaw, 0, new_pitch]], pitch_comp=-40, immediately=False, speed=50)
+                        my_dog.head_move([[yaw, 0, new_pitch]], pitch_comp=0, immediately=False, speed=50)
                         my_dog.wait_head_done()
-
-                        sleep(0.05)
 
                         ex = Vilib.detect_obj_parameter['human_x'] - 320
                         ey = Vilib.detect_obj_parameter['human_y'] - 240
@@ -76,11 +88,35 @@ def face_track():
         if people > 0 and flag == False:
             flag = True
             my_dog.do_action('wag_tail', step_count=2, speed=100)
-            bark(my_dog, [yaw, 0, pitch], pitch_comp=-40, volume=80)
-            my_dog.do_action('stand', step_count=1, speed=70)
+
+            if ex > 15 and yaw > -80:
+                yaw -= 0.5 * int(ex/30.0+0.5)
+
+            elif ex < -15 and yaw < 80:
+                yaw += 0.5 * int(-ex/30.0+0.5)
+
+            if ey > 25:
+                pitch -= 1*int(ey/50+0.5)
+                if pitch < -30:
+                    pitch = -30
+            elif ey < -25:
+                pitch += 1*int(-ey/50+0.5)
+                if pitch > 30:
+                    pitch = 30
+
+            yaw = int(yaw)
+            pitch = int(pitch)
+
+            bark(my_dog, [yaw, 0, pitch], pitch_comp=0, volume=80)
+            my_dog.head_move([[yaw, 0, pitch]], pitch_comp=0, immediately=False, speed=50)
             my_dog.wait_all_done()
+
             if my_dog.ears.isdetected():
                 direction = my_dog.ears.read()
+
+            # input()
+
+            break   
 
         if ex > 15 and yaw > -80:
             yaw -= 0.5 * int(ex/30.0+0.5)
@@ -102,11 +138,60 @@ def face_track():
               end='\r',
               flush=True,
         )
-        my_dog.head_move([[yaw, 0, pitch]], pitch_comp=-40, immediately=True, speed=50)
+        my_dog.head_move([[yaw, 0, pitch]], pitch_comp=0, immediately=True, speed=50)
         my_dog.wait_head_done()
         if my_dog.ears.isdetected():
             direction = my_dog.ears.read()
         sleep(0.05)
+
+    distance = my_dog.read_distance()
+    distance = round(distance,2)
+    print(f"Distance: {distance} cm")
+    print(f"Distance horizontal: {distance * math.cos(math.radians(pitch))} cm")
+    print(f"Pitch: {pitch}")
+    print(f"Yaw: {yaw}")
+
+    if(yaw < 0):
+        for i in range(-yaw // 12):
+            my_dog.do_action('turn_right', speed=80)
+            my_dog.do_action('backward', speed=80)
+    elif(yaw > 0):
+        for i in range(yaw // 12):
+            my_dog.do_action('turn_left', speed=80)
+            my_dog.do_action('backward', speed=80)
+        # my_dog.do_action('turn_left', step_count=yaw // 10, speed=80)
+
+    my_dog.head_move([[0, 0, pitch]], pitch_comp=0, immediately=True, speed=50)
+    
+    my_dog.wait_all_done()
+
+    # ex = Vilib.detect_obj_parameter['human_x'] - 320
+    # ey = Vilib.detect_obj_parameter['human_y'] - 240
+    # people = Vilib.detect_obj_parameter['human_n']
+
+    horizontal_dist = int(distance * math.cos(math.radians(pitch)))
+    for i in range(horizontal_dist // 12):
+        if my_dog.read_distance() < 15 and my_dog.read_distance() > 1:
+            break
+        my_dog.do_action('trot', speed=80)
+        my_dog.wait_all_done()
+
+    # my_dog.wait_all_done()
+
+    my_dog.do_action('sit', speed=80)
+    my_dog.head_move([[0, 0, 0]], pitch_comp=-40, immediately=True, speed=50)
+
+    my_dog.wait_all_done()
+
+    while(True):
+        # relax
+        if my_dog.dual_touch.read() != 'N':
+            if len(my_dog.head_action_buffer) < 2:
+                head_nod(1)
+                my_dog.do_action('wag_tail', step_count=10, speed=80)
+                my_dog.rgb_strip.set_mode('listen', color="#8A2BE2", bps=0.35, brightness=0.8)
+
+    input()
 
 if __name__ == "__main__":
     try:
